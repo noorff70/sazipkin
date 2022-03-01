735e2fa9a8a0aa7caaa48930e9a77d2f4fa187b8
noorf <faizul.noor@toyota.com>
2022-03-01 18:39:22 -0500

    Initial Commit

---
+++ src/main/java/com/sazipkin/SazipkinApplication.java
@@ -0,0 +1,15 @@
+package com.sazipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+
+@SpringBootApplication
+public class SazipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SazipkinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sazipkin/SazipkinApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sazipkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import zipkin.server.EnableZipkinServer;
+
+@EnableZipkinServer
+@SpringBootTest
+class SazipkinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
